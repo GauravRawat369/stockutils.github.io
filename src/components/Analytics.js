65f78c426c4e994bd4032b8a1b935e6c22f4d6d2
Gaurav Rawat <yashu.rawat1903@gmail.com>
2023-07-28 12:20:44 +0530

    api change
    change daily adjusted api to weekly adjusted because it was premium

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -43,9 +43,9 @@ const Analytics = () => {
   const fetchStock = async () => {
     const API_KEY = process.env.API_KEY_FOR_ST_DATA_AND_ST_TOCKEN
     const Stock_Symbol = inputValue;
-    // const Stock_Symbol = "POLYPLEX.BSE"
+    // const Stock_Symbol = "APPH.DEX"
     console.log(Stock_Symbol);
-    const API_Call_Stock = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${Stock_Symbol}&outputsize=full&apikey=${API_KEY}`;
+    const API_Call_Stock = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=${Stock_Symbol}&outputsize=full&apikey=${API_KEY}`;
     try {
       const res_stock = await fetch(API_Call_Stock);
       const data_stock = await res_stock.json();
@@ -56,17 +56,18 @@ const Analytics = () => {
       const stockChartOpenValues = [];
       const stockChartCloseValues = [];
 
-      for (let key in data_stock["Time Series (Daily)"]) {
-        if (key >= "2020-01-01") {
+      for (let key in data_stock["Weekly Adjusted Time Series"]) {
+        if (key >= "2019-01-01") {
+          // console.log("Valid date key:", key);
           stockChartXValuesFunction.push(key);
           stockChartYValuesFunction.push(
-            data_stock["Time Series (Daily)"][key]["4. close"]
+            data_stock["Weekly Adjusted Time Series"][key]["4. close"]
           );
           stockChartOpenValues.push(
-            data_stock["Time Series (Daily)"][key]["1. open"]
+            data_stock["Weekly Adjusted Time Series"][key]["1. open"]
           );
           stockChartCloseValues.push(
-            data_stock["Time Series (Daily)"][key]["4. close"]
+            data_stock["Weekly Adjusted Time Series"][key]["4. close"]
           );
         }
       }
@@ -95,7 +96,7 @@ const Analytics = () => {
       if (noHikeDay === false) {
         console.log("no data with given hike");
       }
-      // console.log(stockChartYValuesFunction);
+      console.log(stockChartYValuesFunction);
       setStockChartXValues(stockChartXValuesFunction);
       setStockChartYValues(stockChartYValuesFunction);
       setStockChartColors(stockChartColors);
